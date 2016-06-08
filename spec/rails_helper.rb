07ae613859760677d5b94686fd573fb2e8f0652f
Kopylov German <roverrr@gmail.com>
2016-06-08 06:15:45 +0300

    Fix coveralls deprecation of formatter initialization

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,10 +1,10 @@
 require 'simplecov'
 require 'coveralls'
 
-SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter[
+SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter::new([
   SimpleCov::Formatter::HTMLFormatter,
   Coveralls::SimpleCov::Formatter
-]
+])
 SimpleCov.start :rails
 
 # This file is copied to spec/ when you run 'rails generate rspec:install'
