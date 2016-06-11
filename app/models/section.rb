16e7cd0311ce0713ac459bf20bba2ce199f38b00
Kopylov German <roverrr@gmail.com>
2016-06-11 14:58:49 +0300

    Remove already unnecessary callback in the section class

--- app/models/section.rb
+++ app/models/section.rb
@@ -9,6 +9,4 @@ class Section < ActiveRecord::Base
   has_many :issue_to_section_connections, :dependent => :destroy
 
   validates :name, :length => { :maximum => Settings.max_string_field_size }, :presence => true
-
-  after_initialize :remove_empty_arrays
 end
